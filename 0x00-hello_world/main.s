8a8036411a57c7fe562897670e4ecf1d2e406cda
RicardoArquimedes <richardcqt28@gmail.com>
2020-06-10 22:33:01 +0000

    intel

--- 0x00-hello_world/main.s
+++ 0x00-hello_world/main.s
@@ -1,17 +1,18 @@
 	.file	"main.c"
+	.intel_syntax noprefix
 	.text
 	.globl	main
 	.type	main, @function
 main:
 .LFB0:
 	.cfi_startproc
-	pushl	%ebp
+	push	ebp
 	.cfi_def_cfa_offset 8
 	.cfi_offset 5, -8
-	movl	%esp, %ebp
+	mov	ebp, esp
 	.cfi_def_cfa_register 5
-	movl	$0, %eax
-	popl	%ebp
+	mov	eax, 0
+	pop	ebp
 	.cfi_restore 5
 	.cfi_def_cfa 4, 4
 	ret
