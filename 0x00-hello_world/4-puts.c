e5c2a78c5c570fb8beda41823ceb09b100b17603
RicardoArquimedes <richardcqt28@gmail.com>
2020-06-10 18:01:20 +0000

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
-/*variable withs puts*/
+/**
+
+ *variable withs puts
+
+*/
 
 int main(void) { 
 
-  puts("Programming is like building a multilingual puzzle,");
-
-    getchar();
-
+  puts("\"\Programming is like building a multilingual puzzle,");
   return (0);
+
 }
