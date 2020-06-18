7d0d13cbe9be5f2c0acb3ec120e2441b850d854d
RicardoArquimedes <richardcqt28@gmail.com>
2020-06-18 17:24:09 +0000

    fixxing task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
+
 /**
- *main - point entry
- *
- *Return: Always 0
+ * main -  checks for checks for a digit (0 through 9).
+ * Return: Always 0.
  */
 int main(void)
 {
-	int i;
+	int x;
 
-	for (i = 1; i <= 100; i++)
+	for (x = 1; x <= 100; x++)
 	{
-		if (i % 15 == 0)
+		if (x % 3 == 0 || x % 5 == 0)
 		{
-			printf("FizzBuzz ");
-		}
-		else if ((i % 3) == 0)
-		{
-			printf("Fizz ");
-		}
-		else if ((i % 5) == 0)
-		{
-			printf("Buzz ");
+			if (x % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (x % 5 == 0)
+			{
+				printf("Buzz");
+			}
 		}
 		else
-		{printf("%d ", i);
+		{
+			printf("%d", x);
+		}
+		if (x != 100)
+		{
+			putchar(' ');
 		}
 	}
-	printf("\n");
+	putchar('\n');
 	return (0);
 }
