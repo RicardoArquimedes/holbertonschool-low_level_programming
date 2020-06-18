8ca8353bee7d551444fa3f754bd09c6d7ae2c5b7
RicardoArquimedes <richardcqt28@gmail.com>
2020-06-18 17:21:08 +0000

    add task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,18 +13,17 @@ int main(void)
 		if (i % 15 == 0)
 		{
 			printf("FizzBuzz ");
-			else if ((i % 3) == 0)
-			{
-				printf("Fizz ");
-				else if ((i % 5) == 0)
-				{
-					printf("Buzz ");
-					else
-					{
-						printf("%d ", i);
-					}
-				}
-			}
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{printf("%d ", i);
 		}
 	}
 	printf("\n");
