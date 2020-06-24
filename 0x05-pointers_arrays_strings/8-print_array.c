870a2263e11655f1e3339f8eed382275a816a23f
RicardoArquimedes <richardcqt28@gmail.com>
2020-06-24 04:18:06 +0000

    add task

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+
+void print_array(int *a, int n)
+{
+	int p;
+
+	for (p = 0; p < n; p++)
+	{
+		printf("%d", a[p]);
+		if (p < n -1)
+		{
+		printf(", ");
+		}
+	}
+	printf("\n");
+}
