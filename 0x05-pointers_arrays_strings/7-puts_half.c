cc0341e6fcbd9a0d9408d3f5985ac6bc566f7630
RicardoArquimedes <richardcqt28@gmail.com>
2020-06-24 03:48:00 +0000

    add task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints half a string
+ * @str: character
+ */
+void puts_half(char *str)
+{
+	int h;
+
+	for (h = 0; str[h] != '\0'; h++)
+	{
+		h = h + 1;
+	}
+
+	for (h = h / 2; str[h] != '\0'; h++)
+
+	{
+	_putchar(str[h]);
+	}
+	_putchar('\n');
+}
