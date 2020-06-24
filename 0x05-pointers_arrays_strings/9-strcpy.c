949e4f919c4f3e89ca36a839120564f6a88853a4
RicardoArquimedes <richardcqt28@gmail.com>
2020-06-24 04:43:13 +0000

    add task

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *_strcpy - copy pointer
+ * @dest: character
+ * @src: character
+ *Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+	return (dest);
+}
