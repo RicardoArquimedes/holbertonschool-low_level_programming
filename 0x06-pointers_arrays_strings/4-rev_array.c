f589793c0f68146da9eae3a2c3e712fff506e511
RicardoArquimedes <richardcqt28@gmail.com>
2020-06-26 01:07:46 +0000

    add task

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,12 +12,11 @@ void reverse_array(int *a, int n)
 	int end, c;
 
 	end = n - 1;
-for (c = 0; c < n / 2; c++)
-{
-	tmp  = a[c];
-	a[c] = a[end];
-	a[end] = tmp;
-
+	for (c = 0; c < n / 2; c++)
+	{
+		tmp  = a[c];
+		a[c] = a[end];
+		a[end] = tmp;
 	end--;
-}
+	}
 }
