c49c239e889f342222e56d2a4fc9a9dce2237413
RicardoArquimedes <richardcqt28@gmail.com>
2020-06-26 04:20:49 +0000

    fix task

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * _lett  - convert to leet 1733 code
+ * leet  - convert to leet 1733 code
  *@s:integer
  *
  * Return: s
@@ -8,18 +8,19 @@
 char *leet(char *s)
 {
 	int ch = 0;
-	while ( s[ch] != '\0')
+
+	while (s[ch] != '\0')
 	{
 		while (s[ch] == 'a' || s[ch] == 'A')
 			s[ch] = '4';
 		while (s[ch] == 'e' || s[ch] == 'E')
-	                s[ch] = '3';
+			s[ch] = '3';
 		while (s[ch] == 'o' || s[ch] == 'O')
-		        s[ch] = '0';
+			s[ch] = '0';
 		while (s[ch] == 't' || s[ch] == 'T')
-		        s[ch] = '7';
+			s[ch] = '7';
 		while (s[ch] == 'l' || s[ch] == 'L')
-		        s[ch] = '1';
+			s[ch] = '1';
 		ch++;
 	}
 	return (s);
