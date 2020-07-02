bf545d45e29c4def06f9f319158da7e4d3353e8c
RicardoArquimedes <richardcqt28@gmail.com>
2020-07-02 00:43:15 +0000

    add task

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - return the value of x 
+ * @x: integer
+ * @y: integer
+ * Return: prototype
+ */
+
+int _pow_recursion(int x, int y)
+{
+
+	if (y < 0)
+		return (-1);
+	if (y != 0)
+		return (x  * _pow_recursion(x, y - 1));
+	else
+		return (1);
+}
