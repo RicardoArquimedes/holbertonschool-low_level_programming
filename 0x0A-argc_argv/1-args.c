324748bbaf5b646392a12fde17f7af0468456255
RicardoArquimedes <richardcqt28@gmail.com>
2020-07-05 00:41:07 +0000

    add task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point number arguments
+ * @argc: number arg
+ * @argv: pointer
+ * Return: 0 (Always Succces)
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
