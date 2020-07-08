94575f4c42d84a3668863da5a402d49e5ac6982e
RicardoArquimedes <richardcqt28@gmail.com>
2020-07-08 03:55:49 +0000

    add task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,51 +1,50 @@
-#include "holberton.h"
 #include <stdlib.h>
 /**
- * str_len - size string
+ * str_len - size
  * @s: string
  * Return: (len)
  */
 int str_len(char *s)
 {
 	int len = 0;
-	int a;
+	int i;
 
-	for (a = 0; s && s[a] != '\0'; a++)
+	for (i = 0; s && s[i] != '\0'; i++)
 	{
 		len++;
 	}
 	return (len);
 }
 /**
- * str_concat - concatenates two strings.
+ * str_concat - concatenates
  * @s1: string
  * @s2: string
- * Return: (D)
+ * Return: (A)
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *D;
-	int a = 0;
-	int b;
-	int c = str_len(s1) + str_len(s2) + 1;
+	char *A;
+	int i = 0;
+	int j;
+	int t = str_len(s1) + str_len(s2) + 1;
 
-	D = malloc(sizeof(char) * c);
-	if (D == NULL)
+	A = malloc(sizeof(char) * t);
+	if (A == NULL)
 	{
 		return (NULL);
 	}
-	for (b = 0; s1 && s1[b] != '\0'; b++)
+	for (j = 0; s1 && s1[j] != '\0'; j++)
 	{
-		D[a] = s1[a];
-		a++;
+		A[i] = s1[i];
+		i++;
 	}
-	for (b = 0; s2 && s2[b] != '\0'; b++)
+	for (j = 0; s2 && s2[j] != '\0'; j++)
 	{
-		D[a] = s2[b];
-		b++;
+		A[i] = s2[j];
+		i++;
 	}
-	D[a] = '\0';
-	return (D);
+	A[i] = '\0';
+	return (A);
 
 
 }
