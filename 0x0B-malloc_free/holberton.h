23b475c2040966751a15843317a8fb66846090a8
RicardoArquimedes <richardcqt28@gmail.com>
2020-07-08 04:17:40 +0000

    add task

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,2 +1,5 @@
 char *create_array(unsigned int size, char c);
 char *str_concat(char *s1, char *s2);
+void print_grid(int **grid, int width, int height);
+int **alloc_grid(int width, int height);
+
