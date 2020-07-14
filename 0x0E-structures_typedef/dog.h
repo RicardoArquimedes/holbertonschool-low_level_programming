02e37a232bebf003013db3338c789ff8d31d211e
RicardoArquimedes <richardcqt28@gmail.com>
2020-07-14 00:44:44 +0000

    add task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Data group of a dog
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif /*DOG_H*/
