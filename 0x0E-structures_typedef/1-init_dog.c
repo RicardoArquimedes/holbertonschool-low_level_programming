e2528868bb6476941f8990623154fcb78427cc1c
RicardoArquimedes <richardcqt28@gmail.com>
2020-07-14 01:26:16 +0000

    add task

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,12 +1,19 @@
 #include "dog.h"
 #include <stdlib.h>
 
+/**
+ * init_dog - Data group of a dog
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ * @d: pointer
+ */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		return;
-       	d->name = name;
+	d->name = name;
 	d->age = age;
 	d->owner = owner;
 
