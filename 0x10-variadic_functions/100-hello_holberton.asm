be42c278fb35a590e3adf25baa3ce04676ed71b3
RicardoArquimedes <richardcqt28@gmail.com>
2020-07-18 20:38:39 +0000

    fix advance

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,16 +1,22 @@
+
 	;;  ----------------------------------------------------------------------------------------
-	;;  Writes "Hola, mundo" to the console using a C library. Runs on Linux.
+	;;  Writes "Hello, Holberton" to the console using only system calls. Runs on 64-bit Linux only.
+	;;  To assemble and run:
 	;;
-	;;      nasm -felf64 hola.asm && gcc hola.o && ./a.out
+	;;      nasm -f elf64 100-hello_holberton.asm && gcc 100-hello_holberton.o -o hello ---> ./hello
 	;;  ----------------------------------------------------------------------------------------
 
-	          global    main
-	          extern    puts
+	global    main
 
 	          section   .text
-main:				; This is called by the C library startup code
-	          mov       rdi, message ; First integer (or pointer) argument in rdi
-	          call      puts	 ; puts(message)
-	          ret			 ; Return from main back into C library wrapper
-message:
-	          db        "Hello, Holberton", 0 ; Note strings must be terminated with 0 in C
+main:	   mov       rax, 1	; system call for write
+	          mov       rdi, 1 ; file handle 1 is stdout
+	          mov       rsi, message ; address of string to output
+	          mov       rdx, 17	 ; number of bytes
+	          syscall		 ; invoke operating system to do the write
+	          mov       rax, 60	 ; system call for exit
+	          xor       rdi, rdi	 ; exit code 0
+	          syscall		 ; invoke operating system to exit
+
+	          section   .data
+message:	  db        "Hello, Holberton", 10
