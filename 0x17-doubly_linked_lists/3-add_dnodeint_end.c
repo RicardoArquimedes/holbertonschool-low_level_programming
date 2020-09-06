770d17be346aba3cccdc41456ee8d3c55e929e8c
RicardoArquimedes <richardcqt28@gmail.com>
2020-09-06 20:30:49 +0000

    fixxing betty_style

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,25 +9,25 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node, *tmp;
+	dlistint_t *new_node, *tmp;
 
-    tmp = *head;
-    if (!head)
-	return (NULL);
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-	return (NULL);
-    new_node->n = n;
-    new_node->next = NULL;
-    new_node->prev = NULL;
-    if (*head == NULL)
-    {
-	*head = new_node;
+	tmp = *head;
+	if (!head)
+		return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	tmp->next = new_node;
+	new_node->prev = tmp;
 	return (new_node);
-    }
-    while (tmp->next != NULL)
-	tmp = tmp->next;
-    tmp->next = new_node;
-    new_node->prev = tmp;
-    return (new_node);
 }
