af42253c668aad55232332ec3b7c44b130262592
RicardoArquimedes <richardcqt28@gmail.com>
2020-09-07 17:42:56 +0000

    fixxing betty style

--- 0x18-dynamic_libraries/holberton.h
+++ 0x18-dynamic_libraries/holberton.h
@@ -1,5 +1,6 @@
 #ifndef HOLBERTON_H
-#define HOBERTONS_H
+#define HOLBERTON_H
+
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
@@ -25,4 +26,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* HOLBERTON_H */
